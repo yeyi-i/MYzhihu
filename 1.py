e456d8b6f56fd616443342dfb9a9f9ca478fe652
YEYI <327979579@qq.com>
2020-03-30 21:15:44 +0800

    trying get all comments info, but it seems need more work

--- 1.py
+++ 1.py
@@ -3,8 +3,6 @@
 from __future__ import unicode_literals, print_function
 
 import os
-import socket
-import _thread
 from zhihu_oauth import ZhihuClient
 
 
@@ -25,10 +23,15 @@ def listener(c_socket, addr):
     print(msgr)
     msg = "server accepted msg: " + msgr + " from server respond.\r\n"
     c_socket.send(msg.encode('utf-8'))
-    # question = client.question(get_question(msgr))
+    question = client.question(get_question('https://www.zhihu.com/question/382545061/answer/1105370864'))
     # print(question.title)
     # print(question.updated_time)
     # print(question.detail)
+
+    # print(question.author.name) # 没有这个author类
+    print(question.title)
+    print(question.detail)
+    print(question.updated_time)
     #
     # answer = client.from_url(msgr)
     # print(answer.author.name)
@@ -42,25 +45,82 @@ def logger(text):
     log.close()
 
 
+author_name = []
+
+
+def converstaion(replies_object):
+    global author_name
+    if replies_object is not None:
+        num = 0
+        for r in replies_object:
+            if num == 3:
+                break
+            if num > 0:
+                author_name.append(r.author.name)
+                converstaion(r.replies)
+                print("  " + r.author.name + " replied "
+                      + author_name[len(author_name) - 1]
+                      + ": " + r.content + "\n")
+            num += 1
+
+
 if __name__ == '__main__':
     client = ZhihuClient()
-    TOKEN_FILE = 'token.pkl'
+    TOKEN_FILE = './static/token.pkl'
     if os.path.isfile(TOKEN_FILE):
         client.load_token(TOKEN_FILE)
     else:
         client.login_in_terminal()
         client.save_token(TOKEN_FILE)
 
-    # me = client.me()
-    logger("socket listener started")
+    # question = client.question(get_question('https://www.zhihu.com/question/382545061/answer/1105370864'))
+    # # print(question.author.name) # 没有这个author类
+    # print(question.title)
+    # print(question.detail)
+    # print(question.updated_time)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((socket.gethostname(), 8888))
-    s.listen(3)
+    answer = client.from_url('https://www.zhihu.com/question/382545061/answer/1105370864')
+    # print(answer.author.name)
+    # print(answer.content)
+    # print(answer.updated_time)
 
-    while True:
-        c, address = s.accept()
-        _thread.start_new_thread(listener, (c, address))
+    num = 0
+    for i in answer.comments:
+        if num == 3:
+            print(author_name)
+            num = 0
+            break
+        print(i.author.name + ": " + i.content + "\n")
+        author_name.append(i.author.name)
+        # for n in i.replies:
+        #     if num == 5:
+        #         num = 0
+        #         break
+        #     if num > 1:
+        #         print("  " + n.author.name + " replied " + i.author.name + ': ' + n.content + "\n")
+        #     for u in n.replies:
+        #         if num == 3:
+        #             num = 0
+        #             break
+        #         if num > 1:
+        #             print("     " + u.author.name + " replied " + n.author.name + ': ' + u.content + "\n")
+        #         num += 1
+        #     num += 1
+        converstaion(i.replies)
+        num += 1
+
+    # print(answer.comment)
+
+    # # me = client.me()
+    # logger("socket listener started")
+    #
+    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s.bind((socket.gethostname(), 8888))
+    # s.listen(3)
+    #
+    # while True:
+    #     c, address = s.accept()
+    #     _thread.start_new_thread(listener, (c, address))
 
     # 爬取答案：
     # answer = client.answer(1071790931)
